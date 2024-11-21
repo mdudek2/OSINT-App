d171e2a978e2a73ea7c49f9c068c473e049f94a1
mdudek2 <143246358+mdudek2@users.noreply.github.com>
2024-11-21 01:29:56 -0600

    Update osint-app.py
    Improved GUI, started work on twitter scraping

--- osint-app.py
+++ osint-app.py
@@ -10,9 +10,10 @@ from tkinter import ttk
 # Author: Maks Dudek
 # Email: mdudek2@hawk.iit.edu
 # Description: This is a simple GUI app which can scrape the top 5 reddit posts and
-# top 5 comments for each of those posts and print them to a txt file for simple
-# data analysis.
-# dependencies: pip, praw, tweepy
+# top 5 comments for each of those posts and print them to a txt file for simple.
+# data analysis. This app can also search for twitter posts and their replies.
+
+# dependencies: pip, praw, twikit
 
 
 # REDDIT SCRAPE FUNCTION
@@ -23,10 +24,10 @@ def reddit_scrape():
 
     # API CREDENTIALS FOR PRAW. LEFT BLANK ON GITHUB FOR SECURITY REASONS.
     # make an account on https://www.reddit.com/prefs/apps to get credentials.
-    username = ""
-    password = ""
-    client_id = ""
-    client_secret = ""
+    username = "Various-Psychology94"
+    password = "M4Xyii278!"
+    client_id = "4F8LeDgM4W6i007Ea8Neiw"
+    client_secret = "jB2_CF5s3rrmhVH7KaUaDwC0m2DEYw"
 
     reddit_instance = praw.Reddit(
         client_id=client_id,
@@ -56,7 +57,7 @@ def reddit_scrape():
         data_file.write("Link:" + current_post)
         data_file.write("Upvotes: " + str(submission.score))
 
-        # Create a Submission instance to look through commnents
+        # Create a Submission instance to look through comments
         submission = reddit.Submission(reddit_instance, url=current_post)
         submission.comments.replace_more(limit=0)
 
@@ -68,19 +69,28 @@ def reddit_scrape():
 
         data_file.close()
 
-#Twitter/X CRAPE FUNCTION
 
-def twitterScrape():
-    #WIP
+# Twitter/X CRAPE FUNCTION
+
+def twitter_scrape():
+    # WIP
     print("")
+
 
 # GUI CODE
 # main window settings
 window = tk.Tk()
 window.title("OSINT APP")
-window.geometry("800x600")
+window.geometry("600x500")
+window.resizable(False, False)
 
 # labels
+
+# Title
+title_label = tk.Label(window, text="Reddit and Twitter Scraper")
+title_label.place(x=90, y=25)
+title_label.config(font=("Arial", 24, "bold"))
+# reddit
 reddit_label = tk.Label(window, text="Subreddit to search in EX: politics")
 reddit_label.place(x=30, y=100)
 reddit_search_label = tk.Label(window, text="Search Query. EX: Trump")
@@ -88,29 +98,48 @@ reddit_search_label.place(x=30, y=150)
 reddit_save_location_label = tk.Label(window, text="Directory to store results")
 reddit_save_location_label.place(x=30, y=200)
 
+# twitter
+twitter_label = tk.Label(window, text="Twitter Search Query EX: Trump")
+twitter_label.place(x=30, y=300)
+twitter_save_location_label = tk.Label(window, text="Directory to store results")
+twitter_save_location_label.place(x=30, y=350)
+
 # entryvars
+
+# reddit
 r_sub = tk.StringVar(value="politics")
 r_search = tk.StringVar(value="Trump")
-r_dir = tk.StringVar(value="C:\\Users\\Maks\\Desktop\\scrapes\\politics.txt")
+r_dir = tk.StringVar(value="C:\\Users\\Maks\\Desktop\\scrapes\\reddit\\politics-Trump.txt")
+t_dir = tk.StringVar(value="C:\\Users\Maks\\Desktop\\scrapes\\twitter\\Trump.txt")
+
+# twitter
+t_query = tk.StringVar(value="Trump")
 
 # entries
+
+# reddit
 reddit_entry = Entry(window, width=29, textvariable=r_sub)
 reddit_entry.place(x=220, y=100)
 reddit_search_entry = Entry(window, width=35, textvariable=r_search)
 reddit_search_entry.place(x=185, y=150)
-reddit_save_entry = Entry(window, width=35, textvariable=r_dir)
+reddit_save_entry = Entry(window, width=60, textvariable=r_dir)
 reddit_save_entry.place(x=185, y=200)
 
-
-# button functions
-def r_button_pressed(r_entry_string):
-    print("click")
-    print(reddit)
-
+# twitter
+twitter_entry = Entry(window, width=29, textvariable=t_query)
+twitter_entry.place(x=220, y=300)
+twitter_save_entry = Entry(window, width=60, textvariable=t_dir)
+twitter_save_entry.place(x=182, y=350)
 
 # buttons
+
+# reddit
 reddit_button = tk.Button(window, text="Scrape for Posts and Comments", command=lambda: reddit_scrape())
 reddit_button.place(x=30, y=250)
+
+# twitter
+twitter_button = tk.Button(window, text="Scrape for Tweats and replies", command=lambda: twitter_scrape())
+twitter_button.place(x=30, y=400)
 
 # start the window loop
 window.mainloop()
